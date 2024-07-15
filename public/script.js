96e8054a6bae5d0b83023a78f7240b5e5f8b05ee
David Marek <macdrevious@Davids-MBP.localdomain>
2024-07-15 15:45:54 -0600

    added console log math

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('alert-button').addEventListener("click", function() {
         alert('Hello, world!');
+
     });
 });
 
@@ -9,4 +10,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function alertMessage(){
     alert('Hello World!');
+    const a = 5;
+    const b = 3;
+    console.log(a + b);
+}
+
+var x = 3
+console.log(x)
+
+const name = "David"
+console.log(name)
+
+var output = x
+
+var y = 5
+
+var output2 = x + y
+
+console.log(output2)
+
+for (let i =0; i<5; i++) {
+    console.log(i);
 }
